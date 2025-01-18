61c3aa04d32a206d1024773a768817ad35ec2f39
Sam Imagine <sam@samimagine.com>
2025-01-19 00:28:15 +0100

    add boolean is checked

--- app/schemas/cards.py
+++ app/schemas/cards.py
@@ -1,9 +1,10 @@
 from pydantic import BaseModel
-from typing import List
+from typing import List, Optional
 
 class Card(BaseModel):
     front: str
     back: str
+    isChecked: Optional[bool] = False
 
 class Deck(BaseModel):
     category: str
